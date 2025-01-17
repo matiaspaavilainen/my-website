ec3607ed9d08512f59895e00e96e37862704b292
Matias Paavilainen <matias.paavilainen@gmail.com>
2025-01-17 11:12:41 +0200

    Chaneg to a pool to handle idle timeouts for DB

--- Backend/models/database.js
+++ Backend/models/database.js
@@ -1,24 +1,18 @@
 import mysql from 'mysql2/promise';
 import 'dotenv/config';
 
-const connection = await mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
 });
-
-try {
-    connection.connect();
-    console.log('Connected to database');
-} catch (err) {
-    console.error('Error connecting: ' + err);
-}
 
 const getAllPhotos = async () => {
     try {
-        const [results, fields] = await connection.query(
+        const [results, fields] = await pool.query(
             'SELECT * FROM photos ORDER BY time_taken DESC',
         );
         return results;
@@ -29,7 +23,7 @@ const getAllPhotos = async () => {
 
 const getRandomPhoto = async () => {
     try {
-        const [results, fields] = await connection.query(
+        const [results, fields] = await pool.query(
             'SELECT * FROM `photos` ORDER BY RAND() LIMIT 1',
         );
         return results;
@@ -40,7 +34,7 @@ const getRandomPhoto = async () => {
 
 const deletePhoto = async (id) => {
     try {
-        await connection.query(
+        await pool.query(
             'DELETE FROM `photos` WHERE `id` = ?',
             [id]
         );
@@ -51,7 +45,7 @@ const deletePhoto = async (id) => {
 
 const insertPhoto = async (time_taken, title, category, file_n, thumb_n) => {
     try {
-        await connection.query(
+        await pool.query(
             'INSERT INTO `photos` (time_taken, title, category, file_n, thumb_n) VALUES (?, ?, ?, ?, ?)',
             [time_taken, title, category, file_n, thumb_n]
         );
