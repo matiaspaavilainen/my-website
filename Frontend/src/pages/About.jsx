a51fedff4439da451c391efac86fb05d5d12583a
Matias Paavilainen <matias.paavilainen@gmail.com>
2025-11-24 19:46:35 +0200

    fixed about page

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -4,19 +4,21 @@ import nc from '../assets/nc.svg';
 
 const About = () => {
     return (
-        <div className='content'>
-            <div className='content text'>
-                <p>A photo gallery website made with React and Node.js. More information on <a href='https://github.com/matiaspaavilainen/my-website' target='_blank'>GitHub</a>. </p>
-            </div>
-            <div className='content text'>
-                <p>
-                    You are free to share and adapt the photos on this website for non-commercial purposes,
-                    as per the <a href="https://creativecommons.org/licenses/by-nc/4.0/" target='_blank'> Creative Commons Attribution-NonCommercial 4.0 International License</a>.
-                    Please provide attribution to Matias Paavilainen and include a link to the source.
-                </p>
-            </div>
-            <div className='copyright'>
-                <img src={cc} width={50} /> <img src={by} width={50} /> <img src={nc} width={50} />
+        <div className='wrapper'>
+            <div className='content'>
+                <div className='content text'>
+                    <p>A photo gallery website made with React and Node.js. More information on <a href='https://github.com/matiaspaavilainen/my-website' target='_blank'>GitHub</a>. </p>
+                </div>
+                <div className='content text'>
+                    <p>
+                        You are free to share and adapt the photos on this website for non-commercial purposes,
+                        as per the <a href="https://creativecommons.org/licenses/by-nc/4.0/" target='_blank'> Creative Commons Attribution-NonCommercial 4.0 International License</a>.
+                        Please provide attribution to Matias Paavilainen and include a link to the source.
+                    </p>
+                </div>
+                <div className='copyright'>
+                    <img src={cc} width={50} /> <img src={by} width={50} /> <img src={nc} width={50} />
+                </div>
             </div>
         </div>
     );
